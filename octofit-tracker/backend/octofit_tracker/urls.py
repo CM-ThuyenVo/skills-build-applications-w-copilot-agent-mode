1db8dc3a488f590bba787c8e9a38bc2c2e75a383
CM-ThuyenVo <thuyenv@cpa-cm.com>
2026-02-16 17:31:11 +0000

    Add URL routes for activities, workouts, and leaderboard; create management command to populate database with test data

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -19,6 +19,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from octofit_tracker import views
 
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'teams', views.TeamViewSet, basename='team')
+router.register(r'activities', views.ActivityViewSet, basename='activity')
+router.register(r'workouts', views.WorkoutViewSet, basename='workout')
+router.register(r'leaderboard', views.LeaderboardViewSet, basename='leaderboard')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
